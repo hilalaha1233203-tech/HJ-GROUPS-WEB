745e29d8e004b2a3419761eafd5c9937bd3f71d0
hilalaha1233203-tech <hilalaha1233203@gmail.com>
2026-09-23 11:28:05 +0530

    fix: declare Node process global for server lint

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ export default defineConfig([
   {
     files: ['**/*.{mjs,cjs}'],
     languageOptions: {
-      globals: globals.node,
+      globals: { ...globals.node, process: 'readonly' },
     },
   },
   {
