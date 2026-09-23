5f1a386383ec03e4dae8288ac0d48a32bb4b285d
hilalaha1233203-tech <hilalaha1233203@gmail.com>
2026-09-23 11:29:13 +0530

    chore: mark intentional read aloud ref synchronization

--- src/App.jsx
+++ src/App.jsx
@@ -3644,7 +3644,7 @@ export function App() {
 
   useEffect(() => {
     readAloudRef.current = readAloud
-  }, [readAloud])
+  }, [readAloud]) // eslint-disable-line react-hooks/exhaustive-deps
 
   /* =======================================================
      READER CLEANUP
